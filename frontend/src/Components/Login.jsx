c6ccf4c160ef77ff3ba20d7a4aa15f8f722ed4b7
Swaraj Jadhav <jadhavswaraj1920@gmail.com>
2023-11-08 09:23:07 +0530

    Updates

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,22 +1,52 @@
-import { useNavigate } from "react-router-dom"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 function Login() {
-
+    const [loginData, setLoginData] = useState({ email: "", password: "" })
     const router = useNavigate();
+    // console.log(loginData, "loginData")
 
-    function gotoRegister(){
-        router('/register')
+    function handleChange(event) {
+        // console.log(event.target.value, event.target.name)
+        setLoginData({ ...loginData, [event.target.name]: event.target.value })
+    }
+
+    async function Login(event) {
+        event.preventDefault();
+        if (loginData.email && loginData.password) {
+            if (loginData.password.length > 7) {
+                try {
+                    // const response = await axios.post('http://localhost:8000/login', { loginData })
+                    const response = { data: { success: true, message: "Login successfull." } }
+                    if (response.data.success) {
+                        alert(response.data.message)
+                        setLoginData({ email: "", password: "" })
+                        router('/')
+                    }
+                } catch (error) {
+                    alert(error.response.data.message)
+                }
+            } else {
+                alert("Password length must be more than 7 digit.")
+            }
+        } else {
+            alert("All fields are mandatory.")
+        }
     }
 
     return (
         <div>
             <h1>Login</h1>
-            <button onClick={gotoRegister}>Register ?</button>
+            <form onSubmit={Login}>
+                <label>Email </label><br />
+                <input type="email" onChange={handleChange} name='email' value={loginData.email} /><br />
+                <label>Password</label><br />
+                <input type="password" onChange={handleChange} name='password' value={loginData.password} /><br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     )
 }
 
-
-
-
-export default Login
+export default Login;
--- test.js
+++ test.js
@@ -1,16 +1,10 @@
-import { useState } from "react";
-
-var number = 12;
-console.log(number) // 12
-number = 87 // re - assign
-number++; // 88
-number--; // 87
-console.log(number) //  87
 
 
-const [irise, setIrise] = useState(12);
 
-// irise = 12
-console.log(irise) // 12
-setIrise(87)
-console.log(irise) // 87
+try {
+
+
+} catch (error) {
+    console.log(error) // "Hi error"
+    alert(error.response.data.message)
+}
